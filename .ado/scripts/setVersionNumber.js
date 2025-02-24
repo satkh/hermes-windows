ee41589bbadee0bbf3c17049510324a919a2b3a9
Vladimir Morozov <vmorozov@microsoft.com>
2025-02-24 13:10:43 -0800

    New build pipeline (#199)
    * New build pipeline
    
    * Use unofficial templates for PR
    
    * Skip building SDL  tags for PR builds
    
    * Update pipeline script names

--- .ado/scripts/setVersionNumber.js
+++ .ado/scripts/setVersionNumber.js
@@ -2,88 +2,105 @@
  * This script sets the version number for the rest of the build.
  * After this script has run, other tasks can use the variable
  * to retrieve the build number for their logic...
- * 
+ *
  * See: https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml
  * for environment variables used in this script to compute the version number.
- */ 
+ */
 const env = process.env;
 
 function main() {
+  const mustPublish = env["MustPublish"] === "True";
+  console.log(`MustPublish: ${mustPublish}`);
 
-    if (env["Build_Reason"] === "PullRequest") {
-        fatalError("Build script is intended for CI pipeline and should not be used for pull requests.");
-    }
+  const { semanticVersion, fileVersion, isTest } = computeVersion();
+  console.log(`Semantic Version: ${semanticVersion}`);
+  console.log(`Windows File Version: ${fileVersion}`);
 
-    const {semanticVersion, fileVersion} = computeVersion();
-    console.log(`Semantic Version: ${semanticVersion}`);
-    console.log(`Windows File Version: ${fileVersion}`);
+  const testPrefix = isTest ? "Test " : "";
+  const publishPrefix = mustPublish ? "CI " : "PR ";
+  if (!fileVersion.startsWith(semanticVersion)) {
+    // Update the pipeline build number to correlate it with the semantic version.
+    console.log(
+      "##vso[build.updateBuildNumber]" +
+        `${testPrefix}${publishPrefix}${fileVersion} -- ${semanticVersion}`
+    );
+  }
 
-    if (!fileVersion.startsWith(semanticVersion)) {
-      // Update the pipeline build number to correlate it with the semantic version.
-      console.log(`##vso[build.updatebuildnumber]${fileVersion} -- ${semanticVersion}`);
-    }
-
-    // Set the variables (as output) so that other jobs can use them.
-    console.log(`##vso[task.setvariable variable=semanticVersion;isOutput=true]${semanticVersion}`);
-    console.log(`##vso[task.setvariable variable=fileVersion;isOutput=true]${fileVersion}`);
+  // Set the variables (as output) so that other jobs can use them.
+  console.log(
+    `##vso[task.setVariable variable=semanticVersion;isOutput=true]${semanticVersion}`
+  );
+  console.log(
+    `##vso[task.setVariable variable=fileVersion;isOutput=true]${fileVersion}`
+  );
 }
 
-function computeVersion() {
-    // Compute base version;
-    const sourceBranch = env["Build_SourceBranch"];
-    if (sourceBranch === "refs/heads/main") {
-      return computeMainVersion();
-    }
-    if (sourceBranch === "refs/heads/rnw/canary") {
-      return computeMainVersion();
-    }
-    if (sourceBranch.startsWith("refs/heads/rnw/0.")) {
-      return computeReleaseVersion();
-    }
+function computeVersion(mustPublish) {
+  const sourceBranch = env["Build_SourceBranch"];
+  const isTest = sourceBranch.includes("1es-pt-migration");
 
-    if (sourceBranch.startsWith("refs/heads/rnw/abi")) {
-      return computeReleaseVersion();
-    }
-    
-    fatalError(`Build script does not support source branch '${sourceBranch}'.`)
+  if (isTest) {
+    return computeCanaryVersion(/*isTest:*/ true);
+  }
+  if (!mustPublish) {
+    return computeCanaryVersion(/*isTest:*/ false);
+  }
+  if (sourceBranch === "refs/heads/main") {
+    return computeCanaryVersion(/*isTest:*/ false);
+  }
+  if (sourceBranch.startsWith("refs/heads/rnw/0.")) {
+    return computeReleaseVersion();
+  }
+
+  fatalError(`Build script does not support source branch '${sourceBranch}'.`);
 }
 
-function computeMainVersion() {
-    const buildNumber = env["Build_BuildNumber"];
-    const buildNumberParts = buildNumber.split(".");
-    if (buildNumberParts.length !== 4
-        || buildNumberParts[0] !== '0'
-        || buildNumberParts[1] !== '0'
-        || buildNumberParts[2].length !== 4
-        || buildNumberParts[3].length < 4
-        || buildNumberParts[3].length > 5) {
-        fatalError(`Unexpected pre-release build number format encountered: ${buildNumber}`)
-    }
+function computeCanaryVersion(isTest) {
+  const buildNumber = env["Build_BuildNumber"];
+  const buildNumberParts = buildNumber.split(".");
+  if (
+    buildNumberParts.length !== 4 ||
+    buildNumberParts[0] !== "0" ||
+    buildNumberParts[1] !== "0" ||
+    buildNumberParts[2].length !== 4 ||
+    buildNumberParts[3].length < 4 ||
+    buildNumberParts[3].length > 5
+  ) {
+    fatalError(
+      `Unexpected pre-release build number format encountered: ${buildNumber}`
+    );
+  }
 
-    const shortGitHash = env["Build_SourceVersion"].substring(0, 8);
+  const shortGitHash = env["Build_SourceVersion"].substring(0, 8);
 
-    return {
-        semanticVersion: `0.0.0-${buildNumberParts[2]}.${buildNumberParts[3]}-${shortGitHash}`,
-        fileVersion: buildNumber
-    }
+  return {
+    semanticVersion:
+      "0.0.0-" +
+      `${buildNumberParts[2]}.${buildNumberParts[3]}-${shortGitHash}`,
+    fileVersion: buildNumber,
+    isTest,
+  };
 }
 
 function computeReleaseVersion() {
-    const buildNumber = env["Build_BuildNumber"];
-    const buildNumberParts = buildNumber.split(".");
-    if (buildNumberParts.length !== 3) {
-        fatalError(`Unexpected release build number format encountered: ${buildNumber}`)
-    }
+  const buildNumber = env["Build_BuildNumber"];
+  const buildNumberParts = buildNumber.split(".");
+  if (buildNumberParts.length !== 3) {
+    fatalError(
+      `Unexpected release build number format encountered: ${buildNumber}`
+    );
+  }
 
-    return {
-        semanticVersion: buildNumber,
-        fileVersion: buildNumber + '.0'
-    }
+  return {
+    semanticVersion: buildNumber,
+    fileVersion: buildNumber + ".0",
+    isTest: false,
+  };
 }
 
 function fatalError(message) {
-    console.log(`##[error]${message}`);
-    process.exit(1);
+  console.log(`##[error]${message}`);
+  process.exit(1);
 }
 
 main();
